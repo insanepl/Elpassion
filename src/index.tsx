cd57023fc02508478a0c3bf027ea98c84de2837a
Oskar Daszkowski <oskar.daszkowski@gmail.com>
2022-01-06 21:14:26 +0100

    Refactor basename prop

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename="/Elpassion">
     <App />
   </BrowserRouter>,
   document.getElementById("root")
